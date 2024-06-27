81573001d72a25ddd83f0486be7c82372c4d662c
CodeOfUsamiWSL <zhang0285@foxmail.com>
2024-06-27 11:15:38 +0800

    feat:finish project

--- run.sh
+++ run.sh
@@ -2,9 +2,16 @@
 
 /usr/local/sbt/sbt package     
 
+hdfs dfs -put ./input/* input/TriangleCounting/  
 ## /home/usami/workU/SparkTasks/TriangleCounting/target/scala-2.12/trianglecounting_2.12-1.0.jar
+
+
 
 /usr/local/spark/bin/spark-submit \
     --class "TriangleCounting" \
-    /home/usami/workU/SparkTasks/TriangleCounting/target/scala-2.12/trianglecounting_2.12-1.0.jar \
+    --conf spark.executor.extraJavaOptions=-XX:+UseG1GC --conf spark.driver.extraJavaOptions=-XX:+UseG1GC \
+    --conf spark.default.parallelism=200 \
+    --executor-memory 50G \
+    --driver-memory 50G \
+    /home/usami/workU/BigDataSystem/SparkTasks/TriangleCounting/target/scala-2.12/trianglecounting_2.12-1.0.jar \
     2>&1
--- src/main/scala/TriangleCounting.scala
+++ src/main/scala/TriangleCounting.scala
@@ -14,8 +14,11 @@ object TriangleCounting {
 
     // 读取输入数据
     val graph = GraphLoader
-      .edgeListFile(sc, "hdfs://localhost:9000/input/TriangleCounting/Cit-HepPh.txt",true)
+      .edgeListFile(sc, "input/TriangleCounting/Cit-HepPh.txt",true)
       .partitionBy(PartitionStrategy.EdgePartition2D)
+
+    // 持久化图
+    graph.persist()
 
     val startTime = System.currentTimeMillis()
     // 运行三角形计数算法
@@ -32,8 +35,11 @@ object TriangleCounting {
 
     val duration = endTime - startTime
     // 输出总的三角形数量
-    println(s"data:Total triangles count: $totalTriangleCount")
-    println(s"data:Time taken: $duration ms")
+    println(s"[data] Total triangles count: $totalTriangleCount")
+    println(s"[data] Time taken: $duration ms")
+
+    // 释放持久化数据
+    graph.unpersist()
     // 停止 SparkSession
     spark.stop()
   }
